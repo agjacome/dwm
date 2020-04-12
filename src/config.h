c7faabf6b86e915c49aa37e172aea7ed4b071d52
Alberto G. Jácome <agjacome@gmail.com>
2020-04-12 17:49:05 +0200

    apply gaplessgrid patch

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
-/* See LICENSE file for copyright and license details. */
+#include "gaplessgrid.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -44,6 +44,7 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+    { "###",      gaplessgrid },
 };
 
 /* key definitions */
@@ -84,6 +85,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+    { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
