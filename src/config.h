e3b87d2c8438cfa666c0eeb9e4f760e193891844
Alberto G. Jácome <agjacome@gmail.com>
2020-04-12 21:41:14 +0200

    adapt fonts and icons configuration

--- src/config.h
+++ src/config.h
@@ -1,46 +1,31 @@
 #include "gaplessgrid.c"
 #include "push.c"
 
-static const char *fonts[] = {
-    "xft:DroidSansMono Nerd Font:pixelsize=12:antialias=true:autohint=true",
-    "xft:JoyPixels:pixelsize=12:antialias=true:autohint=true"
-};
-
-static const char dmenufont[] = "xft:DroidSansMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static const char *fonts[] = { "DroidSansMono Nerd Font:size=10:antialias=true:autohint=true" };
 
 static const char *colors[][3] = {
     /*               fg         bg         border    */
-    [SchemeNorm] = { "#5b626c", "#1d1f21", "#282a2e" }, 
+    [SchemeNorm] = { "#5b626c", "#1d1f21", "#282a2e" },
     [SchemeSel]  = { "#c5c8c6", "#1d1f21", "#f0c674" }
 };
 
-static const unsigned int borderpx  = 2;
-static const unsigned int snap      = 8;
+static const unsigned int borderpx  = 1;
+static const unsigned int snap      = 32;
 static const int focusonwheel       = 0;
 static const int showbar            = 1;
 static const int topbar             = 1;
 static const int viewontag          = 1;
 
-static const int horizpadbar = 2;
-static const int vertpadbar  = 0;
+static const int horizpadbar = 4;
+static const int vertpadbar  = 4;
 
-// static const char *tags[] = { "\uE000", "\uE001", "\uE008", "\uE002", "\uE006", "\uE007" };
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "\uF0AC", "\uF086", "\uF03D", "\uF120", "\uF013", "\uF135" };
 
 static const Rule rules[] = {
-    /* class                        instance    title           tagmask     isfloating  monitor */
-    { "feh",                        NULL,       NULL,           0,          True,       0 },
-    { "Pale moon",                  NULL,       NULL,           1,          False,      0 },
-    { "mpv",                        NULL,       NULL,           4,          True,       0 },
-    { "Spotify",                    NULL,       NULL,           4,          False,      0 },
-    { "Pcmanfm",                    NULL,       NULL,           8,          False,      0 },
-    { NULL,                         NULL,       "LibreOffice",  32,         False,      0 },
-    { "libreoffice-calc",           NULL,       NULL,           32,         False,      0 },
-    { "libreoffice-impress",        NULL,       NULL,           32,         False,      0 },
-    { "libreoffice-startcenter",    NULL,       NULL,           32,         False,      0 },
-    { "libreoffice-writer",         NULL,       NULL,           32,         False,      0 },
-    { "Zathura",                    NULL,       NULL,           32,         False,      0 },
-    { "Gimp",                       NULL,       NULL,           32,         False,      0 }
+    /* class       instance    title    tagmask    isfloating  monitor */
+    { "feh",       NULL,       NULL,    0,         1,          0 },
+    { "mpv",       NULL,       NULL,    0,         1,          0 },
+    { "Pale moon", NULL,       NULL,    1,         0,          0 },
 };
 
 static const float mfact       = 0.50;
@@ -48,17 +33,12 @@ static const int   nmaster     = 1;
 static const int   resizehints = 0;
 
 static const Layout layouts[] = {
-    /* symbol                 arrange function */
-    // { "\uE019 \uE009 \uE019", tile        },
-    // { "\uE019 \uE00C \uE019", bstack      },
-    // { "\uE019 \uE00D \uE019", gaplessgrid },
-    // { "\uE019 \uE00B \uE019", monocle     },
-    // { "\uE019 \uE00A \uE019", NULL        },
-    { "T", tile        },
-    { "B", bstack      },
-    { "G", gaplessgrid },
-    { "M", monocle     },
-    { "F", NULL        },
+    /* symbol arrange function */
+    { "[]=",  tile        },
+    { "TTT",  bstack      },
+    { "HHH",  gaplessgrid },
+    { "[M]",  monocle     },
+    { "><>",  NULL        },
 };
 
 #define MODKEY Mod4Mask
@@ -70,12 +50,12 @@ static const Layout layouts[] = {
 
 #define SHCMD(cmd) { .v = (const char*[]) { "/bin/sh", "-c", cmd, NULL } }
 
-static char        dmenumon[2] = "0"; 
-static const char *dmenucmd[ ] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#1d1f21", "-nf", "#c5c8c6", "-sb", "#f0c674", "-sf", "#1d1f21", NULL };
+static char        dmenumon[2] = "0";
+static const char  dmenufnt[ ] = "DroidSansMono Nerd Font:size=12:antialias=true:autohint=true";
+static const char *dmenucmd[ ] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufnt, "-nb", "#1d1f21", "-nf", "#c5c8c6", "-sb", "#f0c674", "-sf", "#1d1f21", "-p", "Run:", NULL };
 
 static const char *termcmd[] = { "urxvtc", NULL };
 static const char *navgcmd[] = { "palemoon", NULL };
-static const char *filecmd[] = { "pcmanfm", NULL };
 static const char *mailcmd[] = { "urxvtc", "-e", "mutt", NULL };
 static const char *shotcmd[] = { "scrot", "-mq10", NULL };
 static const char *lockcmd[] = { "slock", NULL };
@@ -91,7 +71,6 @@ static Key keys[] = {
     { MODKEY,               XK_w,               spawn,              {.v = navgcmd       } },
     { MODKEY,               XK_e,               spawn,              {.v = mailcmd       } },
     { MODKEY|ShiftMask,     XK_l,               spawn,              {.v = lockcmd       } },
-    { MODKEY,               XK_d,               spawn,              {.v = filecmd       } },
     { 0,                    0xff61,             spawn,              {.v = shotcmd       } },
     { MODKEY,               XK_m,               togglebar,          {0} },
     { MODKEY,               XK_j,               focusstack,         {.i = +1 } },
